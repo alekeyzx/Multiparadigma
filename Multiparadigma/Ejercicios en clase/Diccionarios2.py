39a4d1da6dd5756e8e9d421a78ebc7e4a1b944f0
alekeyzx <alekeyzx@hotmail.com>
2023-02-19 21:23:01 -0600

    AAAA

--- Multiparadigma/Ejercicios en clase/Diccionarios2.py
+++ Multiparadigma/Ejercicios en clase/Diccionarios2.py
@@ -1,8 +1,5 @@
  #diccionario a partir de listas que por cada tupla dentro de la lista, añada una llave 
  
 Lista = [("manzana",1),("pera",2)]
-print(Lista)
 diccionario = dict(Lista)
 print(diccionario)
-lista2 = list(diccionario)
-print(lista2)
